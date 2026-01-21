0808ea5d51fb48737a12f6042e821ac1c4fbd6ab
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-01-21 16:08:25 +0530

    Auth JWT

---
+++ JS/authentication/indexForJWT.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "someRandomToken";
+
+const app = express();
+
+app.use(express.json());
+
+const users= [];
+
+//We don't need to create a getToken function
+
+
+app.post("/signup", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        msg: "You are signed up!!"
+    })
+})
+
+
+app.post("/signin", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const foundUser = users.find(function(u){
+        if(u.username == username && u.password == password)
+        {
+            return true;
+        }
+    })
+        if(foundUser)
+        {
+            const token = jwt.sign({
+                username: username
+            }, JWT_SECRET)
+
+            // u.token = token;    We don't need to store it anywhere because JWT stores the information in itself
+
+
+            res.send({
+                token
+            })
+            console.log(users);
+        }
+        else
+        {
+            res.status(403).send({
+                message:"Invalid username or Password"
+            })
+        }
+    })
+
+
+
+
+
+//Creating an authenticated end point (means request that goes only if user is signin)
+app.get("/me", function(req, res){
+    const token = req.headers.token   //JWT
+    const decodedInformation = jwt.verify(token, JWT_SECRET);  //this will give the original username
+    const username = decodedInformation.username;
+
+    let foundUser = null;
+    users.find(function(u){
+        if(u.username === username)  //unlike tokens, now we will just find the username of the user and not the token from the database
+        {
+            foundUser = u;
+        }
+    })
+    if(foundUser)
+        {
+            res.json({
+                username: foundUser.username,
+                password: foundUser.password
+            })
+        }
+        else
+        {
+            res.send({
+                message: "Invalid token"
+            })
+        }
+})
+
+app.listen(3000);
