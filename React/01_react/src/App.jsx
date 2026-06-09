051d21899e6f67e7a9ea36b3eb66ddba9ff5ee8f
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-10 00:51:13 +0530

    react.js

--- React/01_react/src/App.jsx
+++ React/01_react/src/App.jsx
@@ -1,5 +1,53 @@
 import { useState } from "react";
 
+//Counter in React-
+
+// function App() {
+  
+//   return <div>
+//     Hi there
+  
+//   <Counter></Counter>
+//   </div>
+  
+// }
+
+// function Counter()
+// {
+//   // defining a state
+//   const [count, setCount] = useState(0);
+
+//   function increaseCount()
+//   {
+//     setCount(count + 1);
+//   }
+
+//   function decreaseCount()
+//   {
+//     setCount(count - 1);
+//   }
+
+//   function resetCount()
+//   {
+//     setCount(0);
+//   }
+
+//   //defining a component
+//   return <div>
+//     <h1>{count}</h1>
+//     <button onClick={increaseCount}>Increase Count</button>
+//     <button onClick={decreaseCount}>Decrease Count</button>
+//     <button onClick={resetCount}>Reset Count</button>
+//   </div>
+
+  
+// }
+
+
+
+//Timer in react-
+
+
 function App() {
   
   return <div>
@@ -15,20 +63,10 @@ function Counter()
   // defining a state
   const [count, setCount] = useState(0);
 
-  function increaseCount()
+  setInterval({function ()
   {
     setCount(count + 1);
-  }
-
-  function decreaseCount()
-  {
-    setCount(count - 1);
-  }
-
-  function resetCount()
-  {
-    setCount(0);
-  }
+  }}, 1000);
 
   //defining a component
   return <div>
