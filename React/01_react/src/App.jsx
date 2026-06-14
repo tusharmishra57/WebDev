d026f7e622fb199dc4e66495aaf752eba6c2cff9
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-14 14:56:30 +0530

    cleanup, conditional dependencies, mounting, unmounting

--- React/01_react/src/App.jsx
+++ React/01_react/src/App.jsx
@@ -48,12 +48,57 @@ import { useState, useEffect } from "react";
 //Timer in react-
 
 
+// function App() {
+  
+//   return <div>
+//     Hi there
+  
+//   <Counter></Counter>
+//   </div>
+  
+// }
+
+// function Counter()
+// {
+//   // defining a state
+//   const [count, setCount] = useState(0);  //value of count is 0 only once, after that it will be according to state change because of useState hook.
+
+//   console.log("counter"); //logs everytime state changes (re render).
+
+//   useEffect(function(){
+//     setInterval(function(){
+//       setCount(function(variable){
+//         return variable+1;
+//       });
+//     }, 1000)
+//   }, []);      //[] is a dependency array, useEffect has two arguments
+
+  
+//   return <div>
+//     <h1>{count}</h1>
+//   </div>
+
+  
+// }
+
+
+//conditional rendering, mounting, unmounting, cleanup
+
 function App() {
+
+  // conditional rendering
+  let [counterVisible, setCounterVisible] = useState(true);
+
+  useEffect(function(){
+    setInterval(function(){
+      setCounterVisible(c => !c)
+    }, 5000)
+  }, [])
   
   return <div>
     Hi there
-  
-  <Counter></Counter>
+    {counterVisible &&<Counter></Counter>}  {/*means counter will show only when counterVisible is true after every 5 sec */}
+    Hello!!!!!
   </div>
   
 }
@@ -63,14 +108,20 @@ function Counter()
   // defining a state
   const [count, setCount] = useState(0);  //value of count is 0 only once, after that it will be according to state change because of useState hook.
 
-  console.log("counter"); //logs everytime state changes (re render).
+  console.log("counter before setInterval"); //logs everytime state changes (re render).
 
   useEffect(function(){
-    setInterval(function(){
+    let clock = setInterval(function(){
       setCount(function(variable){
+        console.log("from inside setInterval");
         return variable+1;
       });
     }, 1000)
+
+    return () => {
+      console.log("On unmount");
+      clearInterval(clock);   //Cleanup
+    }
   }, []);      //[] is a dependency array, useEffect has two arguments
 
   
