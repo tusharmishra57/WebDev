25c06611ec3e0887c94785524eccc18d17c048a4
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-21 01:14:34 +0530

    layouts in react (header, outlet, footer)

--- React/02_react_routing/src/App.jsx
+++ React/02_react_routing/src/App.jsx
@@ -1,24 +1,55 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 
 function App()
 {
     return <div>
         <BrowserRouter> 
 
+          <Routes>
+            <Route path = "/" element = {<Layout/>}>
+              <Route path = "/1stRoute" element = {<Component1/>} />
+              <Route path = "/2ndRoute" element = {<Component2/>}  />
+              <Route path = "/" element = {<Component3/>}  />
+
+              <Route path = "*" element = {<Nopage/>} />   {/*if user goes to any undefined route */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    </div>
+}
+
+function Layout()
+{
+  return <div>
+    <Header/>
+     <Outlet/>  {/*this is going to be the body, will change according to the routes that we are on. */}
+    <footer/>
+  </div>
+}
+
+function Header()
+{
+  return <div style = {{backgound: "red"}}>
         {/* we are using link instead of anchor tag because we want to make it a single page application */}
 
           <Link to = "/">Home</Link>   {/* making a href just like anchor tag */}
           <Link to = "1stRoute">1stRoute</Link>
           <Link to = "2ndRoute">2ndRoute</Link>
+        </div>
+}
 
-          <Routes>
-              <Route path = "/1stRoute" element = {<Component1/>} />
-              <Route path = "/2ndRoute" element = {<Component2/>}  />
-              <Route path = "/" element = {<Component3/>}  />
+function Footer()
+{
+  return <div style = {{backgroud: "green"}}>
+    This is the footer
+  </div>
+}
 
-          </Routes>
-        </BrowserRouter>
-    </div>
+function Nopage()
+{
+  return <div>
+    Sorry error, page not found
+  </div>
 }
 
 function Component1()
