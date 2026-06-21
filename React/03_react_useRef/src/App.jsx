c0e38582ebec0bdc57aeb2bd17d34d4d9d77beb6
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-21 23:34:31 +0530

    referencing to a value using useRef

--- React/03_react_useRef/src/App.jsx
+++ React/03_react_useRef/src/App.jsx
@@ -1,28 +1,51 @@
-import {useRef} from 'react'
+import {useRef, useState} from 'react'
 
 function App()
 {
   // referencing to a DOM using useRef
   const inputRef = useRef();
-
   function focusOnInput()
   {
     inputRef.current.focus();   //on clicking submit button, focus on input box
   }
 
 
+  //referencing to a value using useRef
+  const [currentCount, setCurrentCount] = useState(0);
+  const timer = useRef();
+  
+  function startClock()
+  {
+    setInterval(function(){
+      setCurrentCount(c => c + 1);
+    }, 1000);
+    timer.current = value;
+  }
 
-  return <div>
-    <p>Referencing to a DOM using useRef</p>
-    <input type={"text"}  ref={inputRef}></input>
-    <input type={"text"}></input>
+  function stopClock()
+  {
+    clearInterval(timer.current);
+  }
 
-    <button onClick={focusOnInput}>Submit</button>
-    <br /><br />
+  return (
+    <div>
+      <p>Referencing to a DOM using useRef</p>
+      <input type={"text"}  ref={inputRef}></input>
+      <input type={"text"}></input>
+
+      <button onClick={focusOnInput}>Submit</button>
+      <br /><br />
 
 
-    <p>Referencing to a value using useRef.  Starting and stoping a clock</p>
 
-  </div>
+
+      <p>Referencing to a value using useRef.  Starting and stoping a clock</p>
+      {currentCount}
+      <br />
+      <button onClick = {startClock}>Start</button>
+      <button onClick = {StopClock}>Stop</button> 
+
+    </div>
+  )
 }
 export default App
