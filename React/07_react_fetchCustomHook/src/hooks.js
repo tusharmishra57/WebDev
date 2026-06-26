6f555d028e16d585b2cf3cc57eec35607c340713
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-26 17:50:50 +0530

    useFetch

--- React/07_react_fetchCustomHook/src/hooks.js
+++ React/07_react_fetchCustomHook/src/hooks.js
@@ -4,23 +4,25 @@ export function useFetch(url)
 {
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(true);
-
-    useEffect(() => 
-        {
-          setLoading(true);
+    
+        useEffect(() => 
+        { 
           async function getDetails()
           {
+            setLoading(true);
             const response = await fetch(url);
             const json = await response.json();
             setData(json);
             setLoading(false);
           }
-      
+        
+
           getDetails();
         },[url])
 
-      return {
-        data: data,
-        loading: loading
-      }
+
+    return {
+      data: data,
+      loading: loading
+    }
 }
