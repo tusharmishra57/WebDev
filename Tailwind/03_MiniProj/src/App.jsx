290436319749c2ada2c4e66586d456d25231f59e
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-30 20:47:18 +0530

    basic structure of a webinar frontend

--- Tailwind/03_MiniProj/src/App.jsx
+++ Tailwind/03_MiniProj/src/App.jsx
@@ -1,8 +1,35 @@
-import { SideBar } from './sideBar'
 
 function App() {
-  return <div>
-      <SideBar/>
+  return <div className="flex">
+    <SideBar/>
+    <Content/>
+    </div>
+}
+
+function SideBar()
+{
+  return <div className="bg-red-500 w-72 h-screen ">
+
+  </div>
+}
+
+function Content()
+{
+  return <div className="w-full">
+    <div className="bg-black h-30">
+
+    </div>
+    <div className="grid grid-cols-10 gap-8 p-8">
+    <div className="bg-blue-300 rounded-2xl col-span-2 h-48 -translate-y-12 shadow-lg"> 
+
+    </div>
+    <div className="bg-green-300 rounded-2xl col-span-5 h-96 shadow-lg"> 
+
+    </div>
+    <div className="bg-yellow-300 rounded-2xl col-span-3 h-96 shadow-lg"> 
+
+    </div>
+  </div>
     </div>
 }
 
--- Tailwind/03_MiniProj/src/sideBar.jsx
+++ Tailwind/03_MiniProj/src/sideBar.jsx
@@ -1,10 +1,10 @@
 export function SideBar()
 {
-    return <div className="flex  ">
-        <div className="transition-all duration-1000 w-0 md:flex-1 bg-blue-300 h-screen">
+    return <div className="flex">
+        <div className="transition-all duration-1000 w-0 md:flex-1 bg-blue-300 dark:bg-blue-600 h-screen">
             SideBar
         </div>
-        <div className = "flex-4 bg-green-300 h-screen ">
+        <div className = "flex-4 h-screen bg-red-300 dark:bg-red-600 ">
             Content
         </div>
     </div>
