10adf6b23de7c9ef464abe7fa66f6a8d766ef5d6
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-06-30 14:34:13 +0530

    transition sidebar hide below md

--- Tailwind/03_MiniProj/src/sideBar.jsx
+++ Tailwind/03_MiniProj/src/sideBar.jsx
@@ -1,7 +1,7 @@
 export function SideBar()
 {
     return <div className="flex  ">
-        <div className="hidden md:block flex-1 bg-blue-300 h-screnn">
+        <div className="transition-all duration-1000 w-0 md:flex-1 bg-blue-300 h-screen">
             SideBar
         </div>
         <div className = "flex-4 bg-green-300 h-screen ">
