87dce52e3f9f805d7811e97b2e13b27a4148a177
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-07-26 22:38:12 +0530

    connenction string safety

--- Postgres/src/index.ts
+++ Postgres/src/index.ts
@@ -1,6 +1,6 @@
 import {Client} from "pg";
 
-const pgClient = new Client("postgresql://neondb_owner:npg_8EOICfq0Anvy@ep-old-tooth-axigxdry-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+const pgClient = new Client("connnection string");
 
 // const pgClient = new Client({
 //     user: "neondb_owner",
