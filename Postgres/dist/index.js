5d180e9d10f1dc8f12cd6876c073baa2c1ae05a9
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-07-28 17:23:47 +0530

    send sql query from node js

--- Postgres/dist/index.js
+++ Postgres/dist/index.js
@@ -1,7 +1,7 @@
-import {Client} from "pg";
-
-const pgClient = new Client("postgresql://neondb_owner:npg_rawHul3YB7fN@ep-late-hat-ayth2vwx-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const pgClient = new pg_1.Client("postgresql://neondb_owner:npg_rawHul3YB7fN@ep-late-hat-ayth2vwx-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
 // const pgClient = new Client({
 //     user: "neondb_owner",
 //     password: "npg_rawHul3YB7fN",
@@ -10,11 +10,11 @@ const pgClient = new Client("postgresql://neondb_owner:npg_rawHul3YB7fN@ep-late-
 //     database: "neondb",
 //     ssl: true
 // })
-
 async function main() {
     await pgClient.connect();
-
     const response = await pgClient.query("SELECT * FROM users;");
     console.log(response.rows);
-};
-main()
+}
+;
+main();
+//# sourceMappingURL=index.js.map
